0977136a32b2c59dfa3af6ed4b45d8ba0055ed2f
Kaj Berg <kb223aw@student.lnu.se>
2021-05-31 21:46:13 +0200

    dokumentation för funkioner

--- js/components/burgerMenu.js
+++ js/components/burgerMenu.js
@@ -1,21 +1,32 @@
-export const burgerMenuFunc = () => {
+//burgerMenu.js
+
+/**
+ * Funktion för att visa  hamburgarmenyn
+ *
+ */
+const burgerMenuFunc = () => {
     // Hämta in alla element som behövs för hamburgermenyn
     const burgerMenu = document.querySelector('#burger-menu');
     const menu = document.querySelector('#display-menu');
     const removeMenuIcon = document.querySelector('#remove-menu');
 
-    // Funktion för att sätta display värdet på ett element
-    function setDisplayValueOnHtmlElement(element, value) {
+/**
+ * Funktion för att sätta display värdet på ett element
+ *
+ * @param {*} element
+ * @param {*} value
+ */
+function setDisplayValueOnHtmlElement(element, value) {
       element.style.display = value;
     }
     
-    // Denna eventlistener visar menyn vid klick på hamburger-ikonen
+    // Denna eventlyssnare visar menyn vid klick på hamburger-ikonen
     burgerMenu.addEventListener('click', () => {
       setDisplayValueOnHtmlElement(menu, 'block');
       setDisplayValueOnHtmlElement(burgerMenu, 'none');
     });
     
-    // Denna eventlestener döljer menyn vid klick på kryss ikonen
+    // Denna eventlyssnare döljer menyn vid klick på kryss ikonen
     removeMenuIcon.addEventListener('click', () => {
       setDisplayValueOnHtmlElement(menu, 'none');
       setDisplayValueOnHtmlElement(burgerMenu, 'block');
