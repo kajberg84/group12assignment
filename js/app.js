4957a14ba2fcfa7064b70acd6b6ffbe42bbaa845
Kaj Berg <kb223aw@student.lnu.se>
2021-06-01 20:11:17 +0200

    done with functions

--- js/app.js
+++ js/app.js
@@ -1,21 +1,50 @@
 //app.js
-
 import { burgerMenuFunc } from './components/burgerMenu.js'
 
-const inputArtist = document.querySelector('#search-for-artist');
-const inputSong = document.querySelector('#search-for-song');
-const searchButton = document.querySelector('#result');
+const searchButton = document.querySelector('#searchBtn');
+const songText = document.querySelector('#result')
 
-// Funktion för hamburgarmenyn (OBS! Denna är importerad)
+// https://api.lyrics.ovh/v1/led%20zeppelin/stairway%20to%20heaven
+
+
+/**
+ * Creating textcontent from response. 
+ *
+ * @param {*} data
+ */
+function printSong(data){
+  songText.textContent = data
+}
+
+/**
+ * Fetching info from url
+ *
+ * @param { String } url
+ */
+async function fetchSong (url) {
+  let response = await fetch(url)
+  console.log(response)
+  // let jsonData = await response.json()
+
+  printSong(response)
+  }
+
+//  Creating hamburger and appending it
 burgerMenuFunc()
+searchButton.addEventListener('click', (e) => {
+  e.preventDefault()
 
+  // Get input values
+  const inputArtist = document.querySelector('#search-for-artist')
+  const inputSong = document.querySelector('#search-for-song')
 
+   //Build url
+  const URL = "https://swapi.dev/api/starships/12/"
 
-// Det som behöver göras
-// Skapa funktion för fetch
-// Ta hand om informationen från fetchen
-// Skapa funktion för att displaya fetchade datan
-// Skapa logik för knappen
-
-// Skapa funktionalitet för hamburgermenyn (dropdown med våra namn)
-// Skapa html element och lite css, (eventlistner display klass)
+if(inputArtist.value.length === 0 || inputSong.value.length === 0) {
+  songText.textContent = "Fill out both fields"
+  //fixa en funktion som rensar gamla textcontent error då man börjar skriva i fältet igen.
+} else {
+  fetchSong(URL)
+}
+})
